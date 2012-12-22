7dac32d571548ea05c61941d645aa014031306c4
root <root@cloud-102.(none)>
2012-12-22 18:39:45 +0800

    finish createuser

--- response_types.py
+++
@@ -1,6 +0,0 @@
-from spyne.model.complex import Array, Iterable, ComplexModel
-from spyne.model.primitive import Integer, Float, Unicode, Boolean, String
-
-class User(ComplexModel):
-    userid = Integer
-    username = Unicode
--- views.py
+++ views.py
@@ -4,26 +4,28 @@ from spyne.protocol.soap import Soap11
 from spyne.protocol.http import HttpRpc
 from spyne.service import ServiceBase
 from spyne.model.complex import Array, Iterable, ComplexModel
-from spyne.model.primitive import Integer, Float, Unicode, Boolean, String
+from spyne.model.primitive import Integer, Float, Unicode, Boolean, String, AnyDict
 from spyne.server.django import DjangoApplication
-
 from django.views.decorators.csrf import csrf_exempt
-from .response_types import *
 
-from horizon import api
-from glance import client as glance_client
-from keystoneclient.v2_0 import client as keystone_client
-from novaclient.v1_1 import client as nova_client
+from .wic.wic_client import wic_client
+c = wic_client()
 
 class WebService(ServiceBase):
-    @srpc(Integer, Unicode, _returns=User)
-    def get_user(userid, username):
-        res = User(userid=userid, username=username)
+    @srpc(String, Unicode, String, String, String, _returns=AnyDict)
+    def CreateUser(requestId, userName, phoneNumber, email, timestamp):
+        params = {'requestId':requestId}
+        params['CreateUser'] = {
+		'userName':userName,
+		'phoneNumber':phoneNumber,
+		'email':email,
+		'timestamp':timestamp,
+		}
+        res = c.wic_add_user(**params)
         return res
 
 soap_services = csrf_exempt(DjangoApplication(Application([WebService],
         'soap.services',
-        #in_protocol=Soap11(),
-        in_protocol=HttpRpc(), #For test
+        in_protocol=Soap11(),
         out_protocol=Soap11(),
     )))
