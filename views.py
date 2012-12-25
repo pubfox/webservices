4b5c737d1b42aa7e71090e868553216d6e1d1f24
root <root@cloud-102.(none)>
2012-12-25 18:30:22 +0800

    use AnyXml

--- views.py
+++ views.py
@@ -1,27 +1,25 @@
 from spyne.application import Application
-from spyne.decorator import srpc, rpc
+from spyne.decorator import srpc
 from spyne.protocol.soap import Soap11
-from spyne.protocol.http import HttpRpc
 from spyne.service import ServiceBase
-from spyne.model.complex import Array, Iterable, ComplexModel
-from spyne.model.primitive import Integer, Float, Unicode, Boolean, String, AnyDict
+from spyne.model.primitive import AnyDict, AnyXml
 from spyne.server.django import DjangoApplication
+from spyne.util.xml import get_xml_as_object
 from django.views.decorators.csrf import csrf_exempt
 
 from .wic.wic_client import wic_client
 c = wic_client()
 
 class WebService(ServiceBase):
-    @srpc(String, Unicode, String, String, String, _returns=AnyDict)
-    def CreateUser(requestId, userName, phoneNumber, email, timestamp):
-        params = {'requestId':requestId}
-        params['CreateUser'] = {
-		'userName':userName,
-		'phoneNumber':phoneNumber,
-		'email':email,
-		'timestamp':timestamp,
-		}
-        res = c.wic_add_user(**params)
+
+    @srpc(AnyXml, _returns=AnyDict)
+    def CreateUser(xml):
+        print repr(xml)
+        #params = get_xml_as_object(xml, AnyDict)
+        #res = params
+        #print res
+        #res = c.wic_add_user(**params)
+        res = {'a':{'b':'c'}}
         return res
 
 soap_services = csrf_exempt(DjangoApplication(Application([WebService],
