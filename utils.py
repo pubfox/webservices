5ab384b364cbbd6adacb086c4363ce1d81f83b1f
root <root@cloud-102.(none)>
2012-12-27 13:53:28 +0800

    add get_method()

--- utils.py
+++ utils.py
@@ -1,9 +1,19 @@
 from spyne.util.odict import odict
 
 def clear_list(params):
+    '''Change the list with single item to the item value'''
     for k, v in params.items():
         if isinstance(v, list) and len(v) == 1:
             params[k] = v[0]
             if isinstance(v[0], dict) or isinstance(v[0], odict):
                 params[k] = clear_list(v[0])
     return params
+
+def get_method(params):
+    for k in params.keys():
+        if k != 'company' and k != 'requestId':
+            return k
+
+def xml_decrypt(xml):
+
+    return xml
