c027047fa9c57ac53a6c48de2c0dd7c35987c85a
pubfox <admin@pubfox.com>
2013-01-17 14:10:42 +0800

    add METHOD_NAMES

--- settings.py
+++ settings.py
@@ -1,2 +1,27 @@
 ENCRYPT_PASSWORD = 'qweeee'
 ENCRYPT_CLASS_PATH = 'cc.yun.util.Encrypt'
+
+METHOD_NAMES = {
+'Create':'创建虚机',
+'StopHost':'挂起虚机',
+'DelHost':'删除虚机',
+'CreateDisk':'创建磁盘',
+'BindDisk':'绑定/解绑磁盘',
+'DelDisk':'删除磁盘',
+'RestartHost':'重启虚机',
+' ShutdownHost ':'关闭虚机',
+'CreateSecurityGroup':'创建安全组',
+'DescribeSecurityGroup':'查询安全组',
+'DelSecurityGroup':'删除安全组',
+'UpdateNetSpeed':'扩展带宽',
+'UnbindIp':'解绑IP',
+'BindIp':'绑定IP',
+'ApplyIp':'申请/删除IP',
+'UpdateOs':'变更操作系统',
+'CreateSnapshot':'创建快照',
+'DeleteSnapshot':'删除快照',
+'RestoreSnapShot':'恢复快照',
+'CreateImage':'创建镜像',
+'DeleteImage':'删除镜像',
+'StartHost':'开启虚机',
+}
