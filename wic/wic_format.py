f9c975506e0538c3397f882b0f968f49e4fe5a82
root <root@ctrl-01.(none)>
2013-03-23 16:12:52 +0800

    after chuangli test

---
+++ wic/wic_format.py
@@ -0,0 +1,31 @@
+from wic_client_defines import *
+
+def make_default_create_response(kwargs):
+    kwargs["Create"]["CreateHost"]["result"] = WIC_RES_FAILED
+    kwargs["Create"]["CreateHost"]["instanceId"] = None
+    kwargs["Create"]["CreateHost"]["imageId"] = None
+    kwargs["Create"]["CreateHost"]["privateIp"] = None
+    kwargs["Create"]["CreateHost"]["osUserName"] = default_ins_name
+    kwargs["Create"]["CreateHost"]["osPassword"] = default_ins_pass
+    kwargs["Create"]["CreateHost"]["reservationId"] = default_reservationId
+    kwargs["Create"]["CreateHost"]["vmName"] = default_vmname
+    kwargs["Create"]["CreateHost"]["privateDnsName"] = default_privateDnsName
+    kwargs["Create"]["CreateHost"]["dnsName"] = default_dnsName
+    kwargs["Create"]["CreateHost"]["keyName"] = default_keypair
+    kwargs["Create"]["CreateHost"]["amiLaunchIndex"] = None
+    kwargs["Create"]["CreateHost"]["instanceType"] = None
+    kwargs["Create"]["CreateHost"]["placement"] = None
+    kwargs["Create"]["CreateHost"]["kernelId"] = None
+    kwargs["Create"]["CreateHost"]["ramvolumeId"] = 0
+    kwargs["Create"]["CreateHost"]["isEnableHa"] = False
+    kwargs["Create"]["CreateHost"]["vpcId"] = 0
+    kwargs["Create"]["CreateHost"]["mac"] = default_mac
+    kwargs["Create"]["CreateHost"]["ipAddress"] = None
+    kwargs["Create"]["CreateHost"]["rateLimit"] = None
+    kwargs["Create"]["CreateHost"]["vmHostName"] = "ins_" + str(kwargs["requestId"])
+    kwargs["Create"]["CreateHost"]["vncPort"] = 0
+    kwargs["Create"]["CreateHost"]["snapshotId"] = 0
+    kwargs["Create"]["CreateHost"]["sysVolumeId"] = 0
+    kwargs["Create"]["CreateHost"]["note"] = None
+    
+    return kwargs
